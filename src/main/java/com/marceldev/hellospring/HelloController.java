a2da1fa4b2d0e30ff983443b78bf0807a416f380
Marcel Lee <chungchung1315@gmail.com>
2024-11-08 14:13:57 +0900

    feat: Add health check

--- src/main/java/com/marceldev/hellospring/HelloController.java
+++ src/main/java/com/marceldev/hellospring/HelloController.java
@@ -1,5 +1,6 @@
 package com.marceldev.hellospring;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,20 @@ public class HelloController {
   @GetMapping("/hello")
   public String hello() {
     return "hello";
+  }
+
+  @GetMapping("/startup")
+  public ResponseEntity<Void> startup() {
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/readiness")
+  public ResponseEntity<Void> readiness() {
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/liveness")
+  public ResponseEntity<Void> liveness() {
+    return ResponseEntity.ok().build();
   }
 }
